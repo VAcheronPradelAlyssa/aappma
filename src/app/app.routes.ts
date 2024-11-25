21662e47ab87bdd28e7a8f204afbcd28de15906d
VacheronAlyssa <alyssa.vacheron@isatech.fr>
2024-11-25 02:16:35 +0100

    ajout mentions légales base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ActualitesComponent } from './pages/actualites/actualites.component';
 import { ArticleComponent } from './pages/article/article.component';
 import { EvenementsComponent } from './pages/evenements/evenements.component';
+import { MentionsLegalesComponent } from './pages/mentions-legales/mentions-legales.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'actualites', component: ActualitesComponent },
   { path: 'article/:id', component: ArticleComponent }, // Vérifie cette route pour l'article
   { path : 'evenements', component : EvenementsComponent},
+  { path: 'mentions', component: MentionsLegalesComponent },
   { path: '**', redirectTo: '' }
 ];
