a67a89bf0f4e2f49590624cbda08be0855c92ae6
VacheronAlyssa <alyssa.vacheron@isatech.fr>
2025-01-21 16:16:06 +0100

    ajout photos

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -10,18 +10,50 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
-  categories = ['Tous les poissons', 'Brochets', 'Carpes', 'Silures', 'Autres'];
+  categories = ['Tous les poissons', 'Brochets', 'Carpes', 'Silures','Perches','Sandres', 'Autres'];
   selectedCategory = 'Tous les poissons';
 
   images = [
-    { url: '/assets/images/brochets/brochet.jpg', category: 'Brochets', title: 'Brochet pêché à Nantes le 12 juin' },
-    { url: '/assets/images/carpes/antoine.jpg', category: 'Carpes', title: 'Carpe pêchée le 10 janvier à Redon' },
-    { url: '/assets/images/carpes/carpe2.jpg', category: 'Carpes', title: '' }, // Pas de titre
-    { url: '/assets/images/carpes/carpe3.jpg', category: 'Carpes', title: 'Belle carpe au lever du soleil' },
-    { url: '/assets/images/carpes/carpeendu.jpg', category: 'Carpes', title: '' }, // Pas de titre
-    { url: '/assets/images/silures/silure.jpg', category: 'Silures', title: 'Silure géant capturé en rivière' },
-    { url: '/assets/images/silures/silure2.jpg', category: 'Silures', title: '' }, // Pas de titre
-    { url: '/assets/images/autres1.jpg', category: 'Autres', title: 'Un autre poisson impressionnant' },
+    { url: '/assets/images/brochets/brochet.jpg', category: 'Brochets', title: '' },
+    { url: '/assets/images/carpes/antoine.jpg', category: 'Carpes', title: 'Etang du Rodoir' },
+    { url: '/assets/images/carpes/carpe2.jpg', category: 'Carpes', title: '' }, 
+    { url: '/assets/images/carpes/carpe3.jpg', category: 'Carpes', title: 'Enduro 2023' },
+    { url: '/assets/images/carpes/carpeendu.jpg', category: 'Carpes', title: 'Enduro 2023' }, 
+    { url: '/assets/images/carpes/carpeendu4.jpg', category: 'Carpes', title: 'Enduro 2023' },
+    { url: '/assets/images/carpes/carpeendu5.jpg', category: 'Carpes', title: 'Enduro 2023' }, 
+    { url: '/assets/images/carpes/carpeendu6.jpg', category: 'Carpes', title: 'Enduro 2023' }, 
+    { url: '/assets/images/carpes/carpeendu7.jpg', category: 'Carpes', title: 'Enduro 2023' }, 
+    { url: '/assets/images/carpes/carpeendu8.jpg', category: 'Carpes', title: '1er poisson de l\'enduro 2023, 4kg 130' }, 
+    { url: '/assets/images/carpes/carpeendu9.jpg', category: 'Carpes', title: 'Enduro 2023' }, 
+    { url: '/assets/images/carpes/carpeendu10.jpg', category: 'Carpes', title: 'Enduro 2023' }, 
+    { url: '/assets/images/carpes/endu2024.jpg', category: 'Carpes', title: 'Enduro 2024' },
+    { url: '/assets/images/carpes/endu20242.jpg', category: 'Carpes', title: 'Enduro 2024' },
+    { url: '/assets/images/carpes/endu20243.jpg', category: 'Carpes', title: 'Enduro 2024' },
+    { url: '/assets/images/carpes/endu20244.jpg', category: 'Carpes', title: 'Enduro 2024' },
+    { url: '/assets/images/carpes/endu20245.jpg', category: 'Carpes', title: 'Enduro 2024' },
+    { url: '/assets/images/carpes/endu20246.jpg', category: 'Carpes', title: 'Enduro 2024' },
+    { url: '/assets/images/carpes/endu20247.jpg', category: 'Carpes', title: 'Enduro 2024' },
+    { url: '/assets/images/perches/perche.jpg', category: 'Perches', title: '' },
+    { url: '/assets/images/perches/perche2.jpg', category: 'Perches', title: '' },
+    { url: '/assets/images/perches/perche4.jpg', category: 'Perches', title: '' },
+    { url: '/assets/images/perches/perche5.jpg', category: 'Perches', title: '' },
+    { url: '/assets/images/perches/perche7.jpg', category: 'Perches', title: '' },
+    { url: '/assets/images/sandres/sandre2.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre3.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre4.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre5.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre6.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre8.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre9.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre10.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre11.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre12.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre13.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandre14.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/sandres/sandres.jpg', category: 'Sandres', title: '' },
+    { url: '/assets/images/silures/silure.jpg', category: 'Silures', title: '' },
+    { url: '/assets/images/silures/silure2.jpg', category: 'Silures', title: '' },
+    { url: '/assets/images/canard.jpg', category: 'Autres', title: '🦆🦆🦆' }
   ];
 
   filteredImages = [...this.images]; // Liste des images filtrées en fonction de la catégorie sélectionnée
